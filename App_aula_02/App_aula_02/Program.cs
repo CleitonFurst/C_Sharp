53c7839c2c3497002c0022c6cd97d82503a2a969
cleitonfurst <furstcleiton@gmail.com>
2021-07-13 20:23:56 -0300

    first commit

---
+++ App_aula_02/App_aula_02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.WriteLine("Digite um número inteiro :");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+int aux = num;
+while (num != 1)
+{
+    result += result *( num -1);
+    
+    num = num - 1;
+}
+
+Console.WriteLine($"O Fatorial de {aux} é igual á {result}");
